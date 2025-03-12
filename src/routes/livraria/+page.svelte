94a64badc46e431a851ad845e40da07ecbab8d7b
marcelonetodev <marcelo.neto.dev@gmail.com>
2025-03-12 18:28:59 +0000

    Correção de erro no deploy

--- src/routes/livraria/+page.svelte
+++ src/routes/livraria/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-    import Livraria from "../../components/shared/Livraria.svelte";
-    import Titulo from "../../components/shared/Titulo.svelte";
-  </script>
-  
-  <div class="flex flex-col gap-10">
-    <Titulo
-      before="Livros sobre o"
-      key="Verbo"
-      after="Indicações de livros que alimentam a fé e iluminam o coração."
-      />
-      <Livraria />
-  </div>
-  
+  import Livraria from "../../components/shared/Livraria.svelte";
+  import Titulo from "../../components/shared/Titulo.svelte";
+</script>
+
+<div class="flex flex-col gap-10">
+  <Titulo
+    before="Livros sobre o"
+    key="Verbo"
+    after="Indicações de livros que alimentam a fé e iluminam o coração."
+  />
+  <Livraria />
+</div>
